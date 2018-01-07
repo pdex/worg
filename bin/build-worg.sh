b265cea1691076d57901fd525ccce92e518305f8
Chris Keating <Chris.Keating@nordstrom.com>
2018-01-07 12:39:09 -0800

    first attempt at getting worg to build

---
+++ bin/build-worg.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -xe
+
+BUILD_DIR=${BUILD_DIR:-./build}
+
+# create build directory if it doesn't exist
+mkdir -p "$BUILD_DIR"
+
+# build org
+(cd org-mode; make)
+
+# Adapt it to point at your emacs executable
+emacs --batch -l etc/emacs.el -f org-publish-all
